e960240e3ff1ad9f87e7ff7418e10bed4947a16b
Asmitha Karunanithi <asmitk01@in.ibm.com>
2026-03-17 04:25:12 -0500

    vmi: Update static v6 default gateway in bios
    Currently, on configuring the IPv6 static default gateway via redfish,
    the value is set on hypervisor dbus but the corresponding bios
    attribute (vmi_if<0/1>_ipv6_gateway) is not updated. This prevents PLDM
    from picking up the gateway configuration and updating the host
    attribute table to apply it to the hypervisor interface.
    
    The defaultGateway6() method was directly setting the gateway dbus
    property of IP object, which bypasses the gateway() method that contains
    the BIOS table update logic.
    
    Tested-by:
    Verified that vmi_if<0/1>_ipv6_gateway BIOS attribute is now updated
    when static IPv6 gateway is configured via Redfish.
    
    Signed-off-by: Asmitha Karunanithi <asmitk01@in.ibm.com>

--- src/ibm/hypervisor-network-mgr-src/hyp_ethernet_interface.cpp
+++ src/ibm/hypervisor-network-mgr-src/hyp_ethernet_interface.cpp
@@ -721,6 +721,7 @@ static void normalizeGateway(std::string& gw)
 
 std::string HypEthInterface::defaultGateway6(std::string gateway)
 {
+    lg2::error("Setting IPv6 static default gateway");
     normalizeGateway<stdplus::In6Addr>(gateway);
     if (gateway.empty())
     {
@@ -739,8 +740,8 @@ std::string HypEthInterface::defaultGateway6(std::string gateway)
             if (ipObj->origin() == HypIP::AddressOrigin::Static)
             {
                 HypEthernetIntf::defaultGateway6(gateway);
-                // Update ipv6 gateway as well
-                ipObj->HypIP::gateway(gateway);
+                // Update ipv6 gateway as well - this will trigger the BIOS table update
+                ipObj->gateway(gateway);
             }
             else
             {
